6a128dbc59e89cd833e41663d326407f1d3ee9d3
Daniel Do <dodaniel006@gmail.com>
2025-05-20 10:04:45 -0700

    Version 1.0

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,64 @@
 console.log("js file loaded");
 
-async function loadPokemon() {
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=1`);
-    let jsonObj = await response.json();
-    console.log(jsonObj);
-    let pokemon = jsonObj.results[0];
+let count = 0;
+let loading = false;
+let container = document.getElementById("container");
+container.style.display = "none";
+let load = document.getElementById("load");
 
-    console.log(pokemon.name);
-    let response2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
-    let jsonObj2 = await response2.json();
-    console.log(jsonObj2);
-    console.log(jsonObj2.sprites.other['official-artwork'].front_default);
+async function loadPokemon() {
+    if (!loading) {
+        loading = true;
+
+        let card = document.getElementById("pokemonList");
+        let cardArray = [];
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${count}&limit=10`);
+        let jsonObj = await response.json();
+
+        for (pokemon of jsonObj.results) {
+            let response2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+            let jsonObj2 = await response2.json();
+
+            cardArray.push(
+                `<div class="card" style="width: 500px;">
+                    <img src="${jsonObj2.sprites.other['official-artwork'].front_default}" class="card-img-top">
+                    <div class="card-body" style="background-color:#f8f8f8;">
+                        <h3 class="card-title">${pokemon.name}</h3>
+                    </div>
+                </div>
+            `)
+        }
+
+        for (cards of cardArray) {
+            card.innerHTML += cards;
+        }
+
+        count += 10;
+        loading = false;
+    }
+
 }
 loadPokemon();
 
-document.addEventListener("scroll", function () {
-    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-    let clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
-    if (scrollTop + clientHeight >= scrollHeight) {
-        console.log("End of page reached");
-    }
+load.addEventListener("click", async function () {
+
+    container.style.display = "flex";
+    load.style.display = "none";
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    loadPokemon();
+
+    container.style.display = "none";
+    load.style.display = "block";
+
 });
+
+// document.addEventListener("scroll", function () {
+//     let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+//     let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+//     let clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+//     if (scrollTop + clientHeight >= scrollHeight - 200) {
+//         console.log("End of page reached");
+//         loadPokemon();
+//     }
+// });
 
